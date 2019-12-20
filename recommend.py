acdf89db8fbf1683ea9a26ac6e70dbca37968d27
stgong <stefanigong@gmail.com>
2019-12-20 18:38:33 +0100

    commit from local mac 12_20 after normalized lstm embedding

--- recommend.py
+++ recommend.py
@@ -27,22 +27,10 @@ def top_k_recommendations(model, sequence, max_length, embedding_size, n_items, 
         X = np.zeros((1, max_length), dtype=np.int32)  # keras embedding requires movie-id sequence, not one-hot
         X[0, :len(seq_by_max_length)] = np.array([item[0] for item in seq_by_max_length]) # item[0] already get the IDs only
 
-    # else:
-    #     X = np.zeros((1, max_length, n_items), dtype=np.int32)  # input shape of the RNN
-    #     X[0, :len(seq_by_max_length), :] = np.array(
-    #         list(map(lambda x: get_features(x), seq_by_max_length)))
-
-    # Run RNN
-
     output = model.predict_on_batch(X)
     # filter out viewed items
     output[0][[i[0] for i in sequence]] = -np.inf
 
-    # output = model.predict(X, batch_size = 1000)
-
-    # find top k according to output
-    # check n largest probability in output
-    # output[0:1][0][output[0:1][0].argsort()[-10:][::-1]]
     recommendations = list(np.argpartition(-output[0], list(range(k)))[:k])
     rec_prob = [output[0][i] for i in recommendations]
     rec = list(tuple(zip(recommendations, rec_prob)))
@@ -64,7 +52,7 @@ def pop_k_recommendations(model, sequence, max_length, embedding_size, n_items, 
     # Run RNN
 
     # output = model.predict_on_batch(X)
-    training_set_item_popularity = np.load('/Users/xun/Documents/Thesis/Improving-RNN-recommendation-model/Dataset/ks-likes-1y-v2/data/training_set_item_popularity.npy', allow_pickle=True)
+    training_set_item_popularity = np.load('/Users/xun/Documents/Thesis/RNN-model/ks-cooks-1y/data/training_set_item_popularity.npy', allow_pickle=True)
     top_10 = training_set_item_popularity.argsort()[-11:][::-1]
     output = top_10
     # filter out viewed items
@@ -85,28 +73,33 @@ def pop_k_recommendations(model, sequence, max_length, embedding_size, n_items, 
 n_items = 1477
 embedding_size = 100
 max_length = 60
-metrics = {'recall': [],
-           'precision': [],
-           'sps': [],
-           'user_coverage':[],
-           'item_coverage': [],
-           'ndcg': [],
-           'blockbuster_share': []
+metrics = {
+            'recall':[],
+            'precision':[],
+            'sps':[],
+            'sps_short':[],
+            'sps_long':[],
+            'user_coverage':[],
+            'item_coverage':[],
+            'total_item_coverage':[],
+            'uniq_rec':[],
+            'ndcg':[],
+            'blockbuster_share':[],
+            'intra_list_similarity':[]
            }
+
 path = '/Users/xun/Documents/Thesis/RNN-model/'
+
 dirname= "ks-cooks-1y"
 # dirname= "ml-1M"
-# model_name = 'rnn_cce_ml30_bs64_ne50.0_gc100_e8_h50_Ug_lr0.1_nt1.ktf'
+
 model_name = 'rnn_cce_ml60_bs32_ne3378.139_gc100_e100_h100_Ug_lr0.1_nt1.hdf5'
-# model_name = 'rnn_cce_ml30_bs64_ne28.337_gc100_e8_h50_Ug_lr0.1_nt1.ktf'
+
 dataset = DataHandler(dirname=dirname)
 
 model = load_model('/Users/xun/Documents/Thesis/RNN-model/ks-cooks-1y/models/' + model_name)
 
-# model.summary()
-#
-# for layer in model.layers:
-#    print(layer.name, layer.get_weights(), layer.output_shape)
+
 target = []
 rec = []
 rec_l = []
@@ -120,21 +113,6 @@ index = 0
 ev = evaluation.Evaluator(dataset, k=10)
 
 for sequence, user_id in dataset.test_set(epochs=1):
-    # if user_id == '2309':
-    #     # print(len(sequence))
-    #     # sequence = sequence[0:-5]
-    #     # print(len(sequence))
-    #     num_viewed = int(len(sequence) / 2)
-    #     viewed = sequence[:num_viewed]
-    #     viewed = viewed[0:-2]
-    #     print(viewed)
-    #     goal = [i[0] for i in sequence[num_viewed:]]
-    #     input, output, recommendations, rec_prob = top_k_recommendations(model, viewed, max_length, embedding_size, 10)
-    #     print(rec_prob)
-    #     for item in recommendations:
-    #         if item in goal:
-    #             true_positive[user_id].append(item)
-    #     print(true_positive)
     num_viewed = int(len(sequence) / 2)
     viewed = sequence[:num_viewed]
     # print(viewed)
@@ -147,9 +125,9 @@ for sequence, user_id in dataset.test_set(epochs=1):
 
 ############Import the results from implicit model directly and evaluate############
 
-    # with open(path+dirname + '/data/implicit_predictions_als150.pickle', 'rb') as fp:
-    #     implicit_predictions = pickle.load(fp)
-    # recommendations = [item[0] for item in implicit_predictions[index]]
+    with open(path+dirname + '/data/implicit_predictions_als_h100.pickle', 'rb') as fp:
+        implicit_predictions = pickle.load(fp)
+    recommendations = [item[0] for item in implicit_predictions[index]]
 
     ##################### save some data for detail review ###################
     '''
@@ -169,8 +147,6 @@ for sequence, user_id in dataset.test_set(epochs=1):
             true_positive[user_id].append(item)
 
     test_u_id.append(user_id)
-
-    # rec_dict[user_id] = recommendations
     rec_dict[user_id] = recommendations
 
     #
@@ -188,11 +164,16 @@ for sequence, user_id in dataset.test_set(epochs=1):
 
     metrics['recall'].append(ev_i.average_recall())
     metrics['sps'].append(ev_i.sps())
+    # metrics['sps_short'].append(ev_i.sps_short())
+    # metrics['sps_long'].append(ev_i.sps_long())
     metrics['precision'].append(ev_i.average_precision())
     metrics['ndcg'].append(ev_i.average_ndcg())
     metrics['user_coverage'].append(ev_i.user_coverage())
+    metrics['total_item_coverage'].append(ev_i.total_item_coverage())
+    metrics['uniq_rec'].append(ev_i.uniq_rec())
     metrics['item_coverage'].append(ev_i.item_coverage())
     metrics['blockbuster_share'].append(ev_i.blockbuster_share())
+    metrics['intra_list_similarity'].append(ev_i.average_intra_list_similarity())
 
     score[user_id] = [metrics['sps'][-1], metrics['precision'][-1]]
 
@@ -201,7 +182,9 @@ print(len(set(target)), len(set(rec_l)))
 
 
 ev.nb_of_dp = dataset.n_items
-metrics_t = 'sps,recall,precision,item_coverage,user_coverage,ndcg,blockbuster_share'
+metrics_t = 'sps,sps_short,sps_long,recall,precision,uniq_rec,total_item_coverage,item_coverage,user_coverage,ndcg,' \
+            'blockbuster_share,intra_list_similarity'
+# sps_short,sps_long,
 metrics_t = metrics_t.split(',')
 for m in metrics_t:
     if m not in ev.metrics:
@@ -210,12 +193,12 @@ for m in metrics_t:
     print(m + '@' + str(ev.k) + ': ', ev.metrics[m]())
 
 # outfile = path+dirname+'/data/test_u_id.pickle'
-outfile1 = path+dirname+'/data/rec_dict_lstm.pickle'
-outfile2 = path+dirname+'/data/score_dict_lstm.pickle'
-outfile3 = path+dirname+'/data/true_positive_lstm.pickle'
+# outfile1 = path+dirname+'/data/rec_dict_lstm.pickle'
+# outfile2 = path+dirname+'/data/score_dict_lstm.pickle'
+# outfile3 = path+dirname+'/data/true_positive_lstm.pickle'
 
-# # outfile4 = path+dirname+'/data/true_positive_imp.pickle'
-outfile5 = path+dirname+'/data/rnn_predictions.pickle'
+# outfile4 = path+dirname+'/data/true_positive_imp_bpr_h50.pickle'
+# outfile5 = path+dirname+'/data/rnn_predictions.pickle'
 # with open(outfile, 'wb') as fp:
 #     pickle.dump(test_u_id, fp)
 # with open(outfile1, 'wb') as fp:
@@ -224,8 +207,11 @@ outfile5 = path+dirname+'/data/rnn_predictions.pickle'
 #     pickle.dump(score, fp, protocol=pickle.HIGHEST_PROTOCOL)
 # with open(outfile3, 'wb') as fp:
 #     pickle.dump(true_positive, fp, protocol=pickle.HIGHEST_PROTOCOL)
-with open(outfile5, 'wb') as fp:
-    pickle.dump(rec, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+# with open(outfile4, 'wb') as fp:
+#     pickle.dump(true_positive, fp, protocol=pickle.HIGHEST_PROTOCOL)
+# with open(outfile5, 'wb') as fp:
+#     pickle.dump(rec, fp, protocol=pickle.HIGHEST_PROTOCOL)
 #
 # with open(path+dirname+'/data/test_input.pickle', 'wb') as fp:
 #     pickle.dump(test_input, fp)
